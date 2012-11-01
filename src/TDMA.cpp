2642337852482091117e5502f8d77fb48d4267ce
Andres Gomez <andres@desktop>
2012-11-01 11:37:40 +0100

    Cleared debugging messages...
    WORKS!!!!

--- src/TDMA.cpp
+++ src/TDMA.cpp
@@ -76,22 +76,27 @@ void TDMA::schedule(){
 
 	aux = timeA + time_slot;
 
+#if _DEBUG==1
 	if(level==0) {
 	  cout << "TopSched: timeA - " << TimeUtil::convert_us(timeA, relative) << " | time_slot - " << TimeUtil::convert_us(time_slot) << " | aux - " << TimeUtil::convert_us(aux, relative) << " @t=" << TimeUtil::convert_us(TimeUtil::getTime(), relative) << endl;
 	}
+#endif
 
 	ret = sem_timedwait(&preempt_sem, &aux);
 	err_no = errno;
 	sem_post(&timing_sem);
 
+#if _DEBUG==1
 	if(level==1) {
 	  cout << "Level 1 exited timedwait! (returned " << ret << ") @t=" << TimeUtil::convert_us(TimeUtil::getTime(), relative) << endl;
 	}
+#endif
 
-
+#if _DEBUG==1
 	if(level==0) {
 	  cout << "Top Level exited timedwait! (returned " << ret << ") @t=" << TimeUtil::convert_us(TimeUtil::getTime(), relative) << endl;
 	}
+#endif
 
 	//If simulation ended while asleep, break
 	if(sim->isSimulating()==0) {
@@ -99,17 +104,21 @@ void TDMA::schedule(){
 	  break;
 	}
 
+#if _DEBUG==1
 	if(level==0) {
 	  cout << "Top Level posted schedule_sem! @t=" << TimeUtil::convert_us(TimeUtil::getTime(), relative) << endl;
-	}	
+	}
+#endif	
 
 	if(load[active_index] != NULL) {
 	  load[active_index]->deactivate();
 	}
 
+#if _DEBUG==1
 	if(level==0) {
 	  cout << "TopSched: return value - " << ret << " @t=" << TimeUtil::convert_us(TimeUtil::getTime(), relative) << endl;
 	}
+#endif
 
 	//If the call timedout
 	if(ret == -1) {
@@ -139,10 +148,6 @@ void TDMA::schedule(){
 	    exit = true;
 	  }
 
-	  if(level==1) {
-	    cout << "Slot preempted\n";
-	  }
-
 	  sem_post(&schedule_sem);
         }
       }//end of while(exit==false)
@@ -158,61 +163,72 @@ void TDMA::schedule(){
 
 ///This function rewrites the activate method to activate both the scheduler(through its semaphores) as well as its load - this runs in the dispatcher thread
 void TDMA::activate() {
-  // if(state == deactivated) 
-    {
-    sem_wait(&activation_sem);
-
-    sem_post(&schedule_sem);
-    
-    pthread_getschedparam(thread, &policy, &thread_param);
-    thread_param.sched_priority = Priorities::get_sched_pr(level); //server priority
-    pthread_setschedparam(thread, SCHED_FIFO, &thread_param);
-
-    state = activated;
-    
-    sem_post(&activation_sem);
+  if(state == activated) {
+    cout << "TDMA::activate - already activated!\n";
   }
+
+  sem_wait(&activation_sem);
+
+  sem_post(&schedule_sem);
+    
+  pthread_getschedparam(thread, &policy, &thread_param);
+  thread_param.sched_priority = Priorities::get_sched_pr(level); 
+  pthread_setschedparam(thread, SCHED_FIFO, &thread_param);
+
+  state = activated;
+    
+  sem_post(&activation_sem);
 }
 
 ///This function rewrites the deactivate method both the scheduler (through its semaphores) as well as its load
 void TDMA::deactivate() {
   int sts, err_no;
 
-  //if(state == activated)
-    {
+  if(state == deactivated) {
+    cout << "TDMA::deactivate - already deactivated!\n";
+  }
 
-    sem_wait(&activation_sem);
+  sem_wait(&activation_sem);
 
-    if(level==1) {
-      cout << "Top Level woke up at activation_sem! @t=" << TimeUtil::convert_us(TimeUtil::getTime(), relative) << endl;
-    }
+#if _DEBUG==1
+  if(level==1) {
+  cout << "Top Level woke up at activation_sem! @t=" << TimeUtil::convert_us(TimeUtil::getTime(), relative) << endl;
+}
+#endif
 
-    sts = sem_trywait(&timing_sem);
+  sts = sem_trywait(&timing_sem);
 
-    if(level==1) {
-      cout << "Top Level activation_sem return " << sts << endl;
-    }
+#if _DEBUG==1
+  if(level==1) {
+    cout << "Top Level activation_sem return " << sts << endl;
+  }
+#endif
 
-    //If the scheduler isn't timing, no need to do anything (just wait for sem to be freed)
-    if (sts == 0) {
-      //empty
-      sts = 0;
-      sem_post(&timing_sem);
-    }
-    else { 
-      //If the scheduler has timing_sem locked, then it must be preempted
+  //If the scheduler isn't timing, no need to do anything (just wait for sem to be freed)
+  if (sts == 0) {
+    //empty
+    sts = 0;
+    sem_post(&timing_sem);
+  }
+  else { 
+    //If the scheduler has timing_sem locked, then it must be preempted
       sem_post(&preempt_sem);
       //errno = -1;
     } 
 
+#if _DEBUG==1
     if(level==1) {
       cout << "Top Level dectivation @t-" << TimeUtil::convert_us(TimeUtil::getTime(), relative) << endl;
     }
+#endif
+
     sem_wait(&schedule_sem);
 
+#if _DEBUG==1
     if(level==1) {
       cout << "Top Level dectivation after sem_wait @t-" << TimeUtil::convert_us(TimeUtil::getTime(), relative) << endl;
     }
+#endif
 
     //now decrease the priority
     pthread_getschedparam(thread, &policy, &thread_param);
@@ -221,7 +237,6 @@ void TDMA::deactivate() {
 
     state = deactivated;
     sem_post(&activation_sem);
-  }
 }
 
 ///This function handles a new job in its load. Depending on the scheduling, this could change the order of execution.
