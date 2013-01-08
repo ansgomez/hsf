272655984c40cb169a0581821e74081fef195395
Andres Gomez <andres@desktop>
2013-01-08 13:55:51 +0100

    Fixed minor show_missed bug!

--- scripts/octave/show_missed_deadlines.m
+++ scripts/octave/show_missed_deadlines.m
@@ -2,6 +2,14 @@ function show_missed_deadlines(name)
 name_missed = strcat(name,'_deadline_metrics.csv');
 missed_file = strcat(name,'_missed_deadlines.csv');
 
+[s, err, msg] = lstat(name_missed);
+
+if(s.size == 0)
+  fprintf('  Missed Deadlines: \n\n');
+  fprintf("\tNo Missed Deadlines \n");
+  return;
+end;
+
 if (size(csvread(missed_file))==0)
   fprintf('  Missed Deadlines: \n\n');
   fprintf("\tNo Missed Deadlines \n");
