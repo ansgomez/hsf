f6885296c363f6f0b1188a661cb5d29e791774f8
Andres Gomez <andres@desktop>
2013-01-12 13:50:02 +0100

    Added ratemonotonic

---
+++ scripts/hsf/ratemonotonic/flat.php
@@ -0,0 +1,84 @@
+<?php
+
+/**  
+ * EDF - FLAT TOPOLOGY GENERETING SCRIPT
+ *
+ * This script has three parameters:
+ *
+ * Param 1: Simulation time (ms)
+ * Param 2: Periodicity (periodic, periodic_jitter)
+ * Param 3: Initial worker period (normally [50-1000] ms)
+ * Param 4: Number of tasks (normally [2,40])
+ * 
+ * If periodicity=periodic_jitter, then jitter = period/4
+ *
+ */
+
+/************************************************************
+ *                 GLOBAL VARIABLES                         *
+ ************************************************************/
+
+$WCET = 25; //WCET for busy_wait loops
+
+//Input Arguments
+$sim_time_ms = $argv[1]; //simulation time
+$periodicity = $argv[2]; //task periodicity
+$period = $argv[3]; //initial worker period
+$n_tasks = $argv[4]; //number of tasks
+
+//Calculated constants...
+$jitter = ceil($period/4);
+
+/************************************************************
+ *                      XML OUTPUT                          *
+ ************************************************************/
+?>
+
+<simulation name="ratemonotonic">
+
+<duration value="<?php echo $sim_time_ms; ?>" units="ms"/>
+
+<runnable type="scheduler" algorithm="RateMonotonic">
+<?php
+   //Generate Workers
+   for($i=0;$i<$n_tasks;$i++) {
+      generateWorker("busy_wait", $i);
+   }
+?>
+
+</runnable>
+
+</simulation>
+
+
+<?php
+
+/************************************************************
+ *                      FUNCTIONS                           *
+ ************************************************************/
+
+//This function generates one worker of a defined load type
+function generateWorker($task, $i) {
+   global $period;
+   global $jitter;
+   global $periodicity;
+   global $WCET;
+
+   $period_i = $period+3*$i;
+
+   echo "\n";
+?>
+   <runnable type="worker" periodicity="<?php echo $periodicity; ?>" task="<?php echo $task; ?>">
+      <period value="<?php echo $period_i; ?>"  units="ms" />
+<?php
+      if($periodicity==="periodic_jitter") {
+         echo "      <jitter value=\"$jitter\" units=\"ms\" />\n";
+      }
+      if($task==="busy_wait") {
+         echo "      <wcet   value=\"$WCET\" units=\"ms\" />\n";
+      }
+      
+      echo "      <relative_deadline value=\"$period_i\" units=\"ms\" /> ";
+      echo "   </runnable>\n";
+}
+?>
---
+++ scripts/hsf/ratemonotonic/n_sweep.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+#####################
+##   DESCRIPTION   ##
+#####################
+
+#This script performs a sweep of Ntasks under one topology and saves all results.
+
+#Variable 1 -> EDF Topology (flat or tree)
+#Variable 2 -> Max number of tasks (flat), number of levels (tree). range is [2-Nmax]
+
+#####################
+##    CONSTANTS    ##
+#####################
+
+sim_time_ms=1000
+#Simulation time in millis
+#Periodicity of tasks
+periodicity="periodic"
+#Algorithm to experiment with
+algorithm="ratemonotonic"
+#Topology
+topology="flat";
+
+#####################
+##    VARIABLES    ##
+#####################
+
+#Maximum initial period
+period_max=80;
+#Maximum number of tasks
+n_max=2;
+
+#Experiment's short name
+short_name="$algorithm"_"$topology"_exp;
+#Experiment description
+desc="Algorithm: $algorithm\nTopology: $topology\nSweep: N_Tasks [2,"$n_max"]";
+
+#####################
+##    FUNCTIONS    ##
+#####################
+
+#function to measure elapsed time
+function timer()
+{
+    if [[ $# -eq 0 ]]; then
+        echo $(date '+%s')
+    else
+        local  stime=$1
+        etime=$(date '+%s')
+
+        if [[ -z "$stime" ]]; then stime=$etime; fi
+
+        dt=$((etime - stime))
+        ds=$((dt % 60))
+        dm=$(((dt / 60) % 60))
+        dh=$((dt / 3600))
+        printf '%d:%02d:%02d' $dh $dm $ds
+    fi
+}
+
+#####################
+##       MAIN      ##
+#####################
+
+#if old simulation data exists, remove it
+sudo rm -f "$algorithm"* sys.csv alloc.csv deadlines.csv util.csv
+
+#create directory with the shortname
+mkdir -p $short_name
+#create subdirectory with the timestamp of the experiment
+DATE=`date +%2H.%M.%S_%d.%m.%Y`;
+DIR=$short_name/$DATE;
+mkdir $DIR
+
+cd $DIR
+
+#save description
+echo -e $desc > description.txt
+
+echo -e "\n*****     N_SWEEP:      *****"
+echo -e "\nRunning:\n\n$desc"
+
+#save the starting time
+tmr=$(timer)
+
+for (( p=50 ; p <= $period_max ; p=p+10 ))
+do
+  DIR_P=period_"$p";
+  mkdir $DIR_P
+
+  #foreach n_task
+  for (( n=2 ; n <= $n_max ; n++ ))
+  do
+    echo -e "\n###   Simulating: N_tasks = $n & Period = $p ###\n"   
+   
+    #Generate XML
+    php $HSF/scripts/hsf/"$algorithm"/"$topology".php $sim_time_ms $periodicity $p $n > "$algorithm".xml
+
+    #Execute HSF
+    sudo $HSF/bin/hsf $algorithm
+
+    #Calculate all metrics plus figure
+    echo -e "***   Calculating all metrics!  ***"
+    $HSF/bin/calculate all $algorithm > /dev/null
+    #$HSF/bin/simfig $algorithm
+
+    #Copy relevant results
+    echo $n >> n_tasks.csv
+    cat "$algorithm"_utilization.csv >> util.csv
+    cat "$algorithm"_sys_cost_us.csv >> sys.csv
+    cat "$algorithm"_alloc_cost_us.csv >> alloc.csv
+    if [[ -f "$algorithm"_deadline_total.csv ]]; then
+      cat "$algorithm"_deadline_total.csv >> deadlines.csv
+    else
+      echo "0" >> deadlines.csv
+    fi
+
+    #Publish results
+    publish $algorithm
+
+    #Move simulation results
+    DIR_N=$DIR_P/ntasks_"$n"
+    mkdir $DIR_N
+    mv "$algorithm"* $DIR_N/
+  done
+done
+
+#Plot all measures for n_sweep
+#octave --no-window-system -q --eval "plotExperiment(\"Number of Tasks\");"
+
+#compare starting time with current time, and save it
+echo "$(timer $tmr)" > runtime.txt
+
+
